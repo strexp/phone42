8faab782354dbf02933bc89b2b64cb4a27386745
Yukari Chiba <i@0x7f.cc>
2026-06-26 20:21:17 +0800

    disable window switching by touch in calling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,6 +3,7 @@
         v-model="viewStore.currentWindow"
         class="flex-grow-1 w-100 fill-hight h-100"
         style="overflow: hidden"
+        :touch="isCallActive ? false : undefined"
     >
         <v-window-item value="phone" class="h-100">
             <PhoneDialer class="pa-4" />
@@ -13,7 +14,10 @@
         </v-window-item>
 
         <v-window-item value="contacts" class="h-100">
-            <ContactList @call-triggered="switchToPhone" @message-triggered="switchToMessage" />
+            <ContactList
+                @call-triggered="switchToPhone"
+                @message-triggered="switchToMessage"
+            />
         </v-window-item>
 
         <v-window-item value="history" class="h-100">
@@ -23,13 +27,22 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import PhoneDialer from "@/components/PhoneDialer.vue";
 import CallHistory from "@/components/CallHistory.vue";
 import ContactList from "@/components/ContactList.vue";
 import MessageList from "@/components/MessageList.vue";
 import { useViewStore } from "@/stores/viewstore";
+import sipController from "@/utils/sipclient";
+import { CallStatus } from "@/types/call";
 
 const viewStore = useViewStore();
+
+const isCallActive = computed(() =>
+    [CallStatus.IN_CALL, CallStatus.CALLING, CallStatus.HELD].includes(
+        sipController.status.value,
+    ),
+);
 
 const switchToPhone = () => {
     viewStore.currentWindow = "phone";
